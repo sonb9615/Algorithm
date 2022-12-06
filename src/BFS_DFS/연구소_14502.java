e6da9b360ea3b3f70c40ef265ffaebfffd14652c
sonbk <sonb9615@gmail.com>
2022-12-06 15:26:52 +0900

    [22.12.06] 연구소 문제 다시 정리

--- src/BFS_DFS/연구소_14502.java
+++ src/BFS_DFS/연구소_14502.java
@@ -5,21 +5,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+//벽이 3개 모두 세워졌을 때 -> 완전탐색
+// 바이러스가 퍼지고 -> dfs
+// 이후 방문되지 않고 빈 공간 인 곳의 수가 안전영역의 크기-> bfs
+
 public class 연구소_14502 {
 
     static FastReader scan = new FastReader();
     static int n,m,b,ans;
+    static int[][] A, blanck;
     static boolean[][] visited;
-    static int[][] A, blank;
     static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
     static void input(){
         n = scan.nextInt();
         m = scan.nextInt();
-        A = new int[n+1][m+1];
-        blank = new int[n*m+1][2];
-        visited = new boolean[n+1][m+1];
         ans = Integer.MIN_VALUE;
+        visited = new boolean[n+1][m+1];
+        blanck = new int[n*m+1][2];
+        A = new int[n+1][m+1];
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < m+1; j++){
                 A[i][j] = scan.nextInt();
@@ -29,14 +33,14 @@ public class 연구소_14502 {
 
     static void bfs(){
         Queue<Integer> queue = new LinkedList<>();
-
+        // 바이러스가 퍼지는 모든 시작 점을 넣고 시작하자
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < m+1; j++){
                 visited[i][j] = false;
                 if(A[i][j] == 2){
+                    visited[i][j] = true;
                     queue.add(i);
                     queue.add(j);
-                    visited[i][j] = true;
                 }
             }
         }
@@ -44,9 +48,9 @@ public class 연구소_14502 {
         while(!queue.isEmpty()){
             int x = queue.poll();
             int y = queue.poll();
-            for(int k = 0; k < 4; k++){
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
+            for(int i = 0; i < 4; i++){
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
                 if(nx < 0 || ny < 0 || nx > n || ny > m) continue;
                 if(A[nx][ny] != 0) continue;
                 if(visited[nx][ny]) continue;
@@ -67,31 +71,28 @@ public class 연구소_14502 {
         ans = Math.max(ans,cnt);
     }
 
-    // 3개의 벽을 모두 세우는 경우
     static void dfs(int idx, int selected_cnt){
         if(selected_cnt == 3){
             bfs();
             return;
         }
         if(idx > b) return;
-
         //벽을 세우는 경우
-       A[blank[idx][0]][blank[idx][1]] = 1;
+        A[blanck[idx][0]][blanck[idx][1]] = 1;
         dfs(idx + 1, selected_cnt + 1);
-
         //벽을 세우지 않는 경우
-        A[blank[idx][0]][blank[idx][1]] = 0;
+        A[blanck[idx][0]][blanck[idx][1]] = 0;
         dfs(idx + 1, selected_cnt);
+
     }
 
     static void pro(){
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < m+1; j++){
-                // 빈 공간에 벽을 세울 수 있습니다.
                 if(A[i][j] == 0){
-                    b++;
-                    blank[b][0] = i;
-                    blank[b][1] = j;
+                    ++b;
+                    blanck[b][0] = i;
+                    blanck[b][1] = j;
                 }
             }
         }
