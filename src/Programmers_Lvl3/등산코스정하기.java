755c3d3b16e788cd80774c8d300d0b8aca2f91ae
sonbk <sonb9615@gmail.com>
2023-01-30 21:58:17 +0900

    [23.01.30] 다익스트라

---
+++ src/Programmers_Lvl3/등산코스정하기.java
@@ -0,0 +1,111 @@
+package Programmers_Lvl3;
+
+import java.util.*;
+
+public class 등산코스정하기 {
+
+    static class Corn{
+        int to, dist;
+        public Corn(int _to, int _dist){
+            this.to = _to;
+            this.dist = _dist;
+        }
+    }
+
+    static class Info{
+        int idx, weight;
+        public Info(int _idx, int _weight){
+            this.idx = _idx;
+            this.weight = _weight;
+        }
+    }
+
+    static ArrayList<Corn>[] adj;
+    static int[] dist;
+    static boolean[] ends;
+    static boolean[] starts;
+
+    public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        int[] answer = new int[2];
+        adj = new ArrayList[n+1];
+        ends = new boolean[n+1];
+        starts = new boolean[n+1];
+        for(int i = 1; i < n+1; i++) adj[i] = new ArrayList<>();
+        for(int i = 0; i < paths.length; i++){
+            int x = paths[i][0];
+            int y = paths[i][1];
+            int dist = paths[i][2];
+            adj[x].add(new Corn(y, dist));
+            adj[y].add(new Corn(x, dist));
+        }
+
+        for(int i = 0; i < summits.length; i++){
+            ends[summits[i]] = true;
+        }
+        for(int i = 0; i < gates.length; i++){
+            starts[gates[i]] = true;
+        }
+
+        Arrays.sort(summits);
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < gates.length; i++){
+            dijkstra(gates[i], n);
+            for(int j = 0; j < summits.length; j++){
+                if(min > dist[summits[j]]){
+                    min = dist[summits[j]];
+                    answer[0] = summits[j];
+                    answer[1] = min;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void dijkstra(int start, int n){
+
+        boolean[] visited = new boolean[n+1];
+        dist = new int[n+1];
+        for(int i = 1; i < n+1; i++) dist[i] = Integer.MAX_VALUE;
+        Queue<Info> queue = new LinkedList<>();
+        queue.add(new Info(start,0));
+        dist[start] = 0;
+        visited[start] = true;
+        while(!queue.isEmpty()){
+            Info info = queue.poll();
+            if(info.weight != dist[info.idx]) continue;
+            for(Corn corn : adj[info.idx]){
+                if(starts[corn.to]) continue;
+                if(visited[corn.to]) continue;
+                if(ends[corn.to]){
+                    int x = Math.max(corn.dist, dist[info.idx]);
+                    if(dist[corn.to] > x){
+                        dist[corn.to] = x;
+                    }
+                }else{
+                    dist[corn.to] = Math.max(corn.dist, dist[info.idx]);
+                    visited[corn.to] = true;
+                    queue.add(new Info(corn.to, dist[corn.to]));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        int n = 7;
+//        int[][] paths = {{1, 4, 4}, {1, 6, 1}, {1, 7, 3}, {2, 5, 2}, {3, 7, 4}, {5, 6, 6}};
+//        int[] gates = {1};
+//        int[] summits = {2,3,4};
+
+//        int n = 6;
+//        int[][] paths = {{1, 2, 3}, {2, 3, 5}, {2, 4, 2}, {2, 5, 4}, {3, 4, 4}, {4, 5, 3}, {4, 6, 1}, {5, 6, 1}};
+//        int[] gates = {1,3};
+//        int[] summits = {5};
+
+        int n = 2;
+        int[][] paths = {{1, 2, 1}};
+        int[] gates = {1};
+        int[] summits = {2};
+        solution(n,paths,gates,summits);
+    }
+}
