e115683ab80a28da915feed74b26a8eda61baf64
sonbk <sonb9615@gmail.com>
2023-02-26 14:11:47 +0900

    [23.02.26] 제한사항을 잘 확인하자

---
+++ src/Programmers_Lvl2/숫자블록.java
@@ -0,0 +1,35 @@
+package Programmers_Lvl2;
+
+public class 숫자블록 {
+
+    public static int[] solution(long begin, long end) {
+        int[] answer = new int[Long.valueOf(end - begin + 1).intValue()];
+        int idx = -1;
+        for(int i = (int)begin; i < (int)end+1; i++){
+            answer[++idx] = getBlock(i);
+        }
+        return answer;
+    }
+
+    public static int getBlock(int x){
+        if(x == 1) return 0;
+        int val = 1;
+        int max = 10000000;
+        for(int i = 2; i <= Math.sqrt(x); i++){
+            if(x % i == 0){
+                val = x/i;
+                if(val <= max) break;
+                else val = i;
+            }
+        }
+        return val;
+    }
+
+    public static void main(String[] args) {
+//        int [] ans = solution(100000014, 100000016);
+        int [] ans = solution(1,10);
+        for(int x : ans){
+            System.out.println(x);
+        }
+    }
+}
