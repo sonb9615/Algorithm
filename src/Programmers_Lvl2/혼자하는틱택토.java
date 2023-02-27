fbe5388e5ee4f1ed41741e30cd06cd62a80b35f6
sonbk <sonb9615@gmail.com>
2023-02-28 01:44:55 +0900

    [23.02.28] 구현 : 문제를 자세히 잘 읽자..

--- src/Programmers_Lvl2/혼자하는틱택토.java
+++ src/Programmers_Lvl2/혼자하는틱택토.java
@@ -5,41 +5,61 @@ public class 혼자하는틱택토 {
     public static int solution(String[] board) {
         int answer = 1;
         int[] arr = new int[2];
-        int crsCnt = 0;
-        int[] cnt = new int[5];
         for(int i = 0; i < 3; i++){
             String str = board[i];
-            int chk = 0;
             for(int j = 0; j < 3; j++){
                 if(str.charAt(j) == '.') continue;
                 if(str.charAt(j) == 'O') ++arr[0];
                 else ++arr[1];
-                ++chk;
-                ++cnt[j];
-                if(i == j) ++cnt[3];
-                if(i == 0 && j == 2 || i == 1 && j == 1 || i == 2 && j == 0) ++ cnt[4];
-            }
-            if(chk == 3) ++crsCnt;
-        }
-
-        for(int x : cnt){
-            if(x == 3) ++crsCnt;
-        }
-
-        if(arr[0] < arr[1]) return 0;
-        if(arr[0] + arr[1] > 6) return 0;
-        if(arr[0] == arr[1]){
-            if(arr[0] > 3) return 1;
-            else{
-                if(crsCnt > 1) return 0;
             }
         }
-        if(arr[0] != arr[1] && arr[0] != arr[1] + 1) return 0;
+
+        if(arr[0] - arr[1] < 0 || arr[0] - arr[1] > 1) return 0;
+        if(arr[0] >= 3){
+            if(isSame(board,'O')){
+                if(arr[0] - arr[1] == 1) return 1;
+                else return 0;
+            }
+        }
+        if(arr[1] >= 3){
+            if(isSame(board,'X')){
+                if(arr[0] - arr[1] == 0) return 1;
+                else return 0;
+            }
+        }
         return answer;
     }
 
+    public static boolean isSame(String[] board, char c){
+
+        for(int i = 0; i < 3; i++){
+            boolean chk = true;
+            for(int j = 0; j < 3; j++){
+                if(board[i].charAt(j) != c) chk = false;
+            }
+            if(chk) return true;
+        }
+
+        for(int i = 0; i < 3; i++){
+            boolean chk = true;
+            for(int j = 0; j < 3; j++){
+                if(board[j].charAt(i) != c) chk = false;
+            }
+            if(chk) return true;
+        }
+
+        if(board[0].charAt(0) == c && board[1].charAt(1) == c && board[2].charAt(2) == c){
+            return true;
+        }
+        if(board[0].charAt(2) == c && board[1].charAt(1) == c && board[2].charAt(0) == c){
+            return true;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        String[] board = {"OOO", "...", "XXX"};
+        String[] board = {"OOO", "OOX", "XXX"};
         System.out.println(solution(board));
     }
 }
