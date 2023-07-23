e1de8d081d6b3587fdafc2275efc8683163aba5b
sonb9615 <sonb9615@gmail.com>
2023-07-23 19:39:05 +0900

    [23.07.23] 백트래킹

---
+++ src/백트레킹/N과M_12_15666.java
@@ -0,0 +1,97 @@
+package 백트레킹;
+
+import java.io.*;
+import java.util.*;
+
+public class N과M_12_15666 {
+
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static ArrayList<Integer> arr;
+    static int[] selected;
+    static Map<String, Integer> map;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        arr = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            arr.add(scan.nextInt());
+        }
+        Collections.sort(arr);
+        selected = new int[M];
+        map = new HashMap<>();
+    }
+    static void ref_func(int k, int start){
+        if(k == M){
+            StringBuilder temp = new StringBuilder();
+            for(int i = 0; i < M; i++){
+                temp.append(selected[i]).append(' ');
+            }
+            if(!map.containsKey(temp.toString())){
+                map.put(temp.toString(), 1);
+                sb.append(temp.toString()).append('\n');
+            }
+        }else{
+            for(int i = start; i < N; i++){
+                selected[k] = arr.get(i);
+                ref_func(k+1, start);
+                ++start;
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        ref_func(0, 0);
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
