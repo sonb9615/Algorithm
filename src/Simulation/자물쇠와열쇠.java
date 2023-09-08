d4b9bf08ed56deaeda356e8616e8fbfcc0f43077
sonb9615 <sonb9615@gmail.com>
2023-09-08 19:08:11 +0900

    [23.09.08] 우선순위큐

---
+++ src/Simulation/자물쇠와열쇠.java
@@ -0,0 +1,39 @@
+package Simulation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 자물쇠와열쇠 {
+    public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        int[][] board = new int[lock.length + (key.length * 2)][lock.length + (key.length * 2)];
+
+        return answer;
+    }
+
+
+
+
+
+    public boolean check(int[][] key, int[][] lock, int[][] board){
+        boolean chk = false;
+        for(int i = 1; i < board.length-1; i++){
+            for(int j = 1; j < board[i].length-1; i++){
+
+            }
+        }
+        Map<String, Integer> map = new HashMap<>();
+
+        return false;
+    }
+
+    public boolean moveKey(int[][] key, int r, int c){
+
+        for(int i = 0; i < key.length; i++){
+            for(int j = 0; j < key.length; j++){
+
+            }
+        }
+        return false;
+    }
+}
